bd2e8d7a2cbd7ce1475a1c662c5a26e1639424f3
ManuDiop <emmanuel.diop@gmail.com>
2024-11-12 11:54:09 +0100

    formulaire de connexion et redirections user

--- bibliographie/urls.py
+++ bibliographie/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('login/', views.login, name='login'), 
-    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'), 
+    path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- bibliographie/views.py
+++ bibliographie/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Livre
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Vous êtes connecté.')
+            return redirect('homepage')
+        else:
+            messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrects.')
+
+    return render(request, 'bibliographie/login.html')
+
+def signup_view(request):
+    return render(request, 'bibliographie/signup.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Vous êtes déconnecté.')
+    return redirect('homepage')
 
 def homepage(request):
     livres = Livre.objects.all()
     return render(request, 'bibliographie/homepage.html', {'livres': livres})
-
-def login(request):
-    return render(request, 'bibliographie/login.html')
-
-def signup(request):
-    return render(request, 'bibliographie/signup.html')
