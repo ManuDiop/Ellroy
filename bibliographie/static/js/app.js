ac5f2ff574ecefa8af842c380cc03aa3456a215e
ManuDiop <emmanuel.diop@gmail.com>
2024-11-25 12:30:36 +0100

    Section commentaires #2

--- bibliographie/static/js/app.js
+++ bibliographie/static/js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM chargé correctement !");
     
-    document.querySelectorAll('.delete-comment').forEach(button => {
+    document.querySelectorAll('#delete-comment').forEach(button => {
         button.addEventListener('click', function(event) {
             if (!confirm('Voulez-vous vraiment supprimer ce commentaire ?')) {
                 event.preventDefault();
