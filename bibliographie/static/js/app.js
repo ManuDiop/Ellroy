9b241e1b67744105e17a89f61ea7fdcc274fa118
ManuDiop <emmanuel.diop@gmail.com>
2024-11-27 18:33:54 +0100

    gestion du Tooltip

--- bibliographie/static/js/app.js
+++ bibliographie/static/js/app.js
@@ -2,6 +2,7 @@ app = {
     init: function() {
         this.smoothScroll();
         this.deleteComment();
+        this.logoutTooltip();
     },
 
     smoothScroll : function() {
@@ -26,7 +27,33 @@ app = {
         });
     },
 
-}
+    logoutTooltip: function() {
+        const logoutIcon = document.getElementById('logoutIcon');
+
+        if (logoutIcon) {
+            logoutIcon.addEventListener('mouseenter', function() {
+                //création de l'élément pour afficher le message
+                const tooltip = document.createElement('div');
+                tooltip.textContent = 'Se déconnecter';
+                tooltip.classList.add('js-tooltip');
+                document.body.appendChild(tooltip);
+
+                // Position de l'élément
+                const rect = logoutIcon.getBoundingClientRect();
+                tooltip.style.left = `${rect.left + window.scrollX + rect.width / 2 - tooltip.offsetWidth / 2}px`;
+                tooltip.style.top = `${rect.bottom + window.scrollY + 10}px`;
+            });
+        
+            logoutIcon.addEventListener('mouseleave', function() {
+                const tooltip = document.querySelector('.js-tooltip');
+                if (tooltip) {
+                    tooltip.remove();
+                }
+            });
+        }
+    }
+};    
+
 
 document.addEventListener('DOMContentLoaded', function() {
     app.init();
